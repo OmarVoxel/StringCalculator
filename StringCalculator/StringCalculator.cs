d07d7ad958e8d75de57f90ec8092b7561c44215f
oizquierdo <Oizquierdo@voxelgroup.net>
2021-12-01 00:42:19 +0100

    feat: SequenceReturnsTheSumBetweenThemIgnoringTheCustomSeparator

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace StringCalculator
 {
@@ -14,11 +15,19 @@ namespace StringCalculator
         {
             if (_sequence.IndexOf(",\n") != -1 || _sequence.IndexOf("\n,") != -1)
                 throw new Exception("Sequence not valid");
-            
+
             if (string.IsNullOrEmpty(_sequence))
                 return 0;
+
+            bool separatorExists = Regex.IsMatch(_sequence, @"^\/\/.\d");
+            char separator = ',';
+
+            if (separatorExists) {
+                separator = _sequence[2];
+                _sequence = _sequence.Substring(3);
+            }
             
-            return _sequence.Split(new char[] { '\n', ',' }).Sum(x => Int32.Parse(x));
+            return _sequence.Split(new char[] { '\n', separator }).Sum(x => Int32.Parse(x));
         }
     } 
 }
