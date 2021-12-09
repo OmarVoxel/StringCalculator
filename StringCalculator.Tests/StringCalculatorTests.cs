3aa57cc52c8929ea2e92ba2d83becc9063687757
oizquierdo <Oizquierdo@voxelgroup.net>
2021-12-09 20:09:14 +0100

    style: clean imports not used and formating the whole code

--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -1,6 +1,5 @@
 using System;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Xunit;
 
 namespace StringCalculator.Tests
@@ -62,8 +61,7 @@ namespace StringCalculator.Tests
         [InlineData("//;-1;-1;3;1;34;-1", "negatives not allowed -1,-1,-1")]
         [InlineData("//.-2.3.-0", "negatives not allowed -2,-0")]
         [InlineData("//;1\n-2;0", "negatives not allowed -2")]
-        //[InlineData("//--1\n--2;-0", "negatives not allowed -1,-2")] this test need to be fixed yet.
-        
+
         public void NegativeNotAllowed(string input, string message)
         {
             StringCalculator stringCalculator = new (input);
